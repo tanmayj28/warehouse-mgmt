51a5a9ccffafba166e29cfec1c55a6d4f76e3021
7anminator <tanmay@happyness.net>
2020-03-16 22:07:07 +0530

    adds rails helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@
 require 'factory_bot_rails'
 require 'faker'
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
