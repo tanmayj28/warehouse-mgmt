ca4423fca1639d6b85683dc68315851e2d9ca371
7anminator <tanmay@happyness.net>
2020-03-17 19:42:39 +0530

    adds specs for warehouse and products model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,45 +3,19 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   before(:each) do
     @product = FactoryBot.create(:product)
-    @warehouse = FactoryBot.create(:warehouse)
   end
 
-  attr_reader :product, :warehouse
+  attr_reader :product
 
   it 'has a valid object' do
-    FactoryBot.create(:inventory, warehouse_code: warehouse.wh_code, product_code: product.sku_code).should be_valid
+    FactoryBot.create(:product).should be_valid
   end
 
-  it 'is invalid without a name' do
-    FactoryBot.build(:amenity, name: nil).should_not be_valid
+  it 'is invalid without a code' do
+    FactoryBot.build(:product, sku_code: nil).should_not be_valid
   end
 
-  it 'is invalid with a duplicate name' do
-    FactoryBot.create(:amenity, name: 'foo')
-    FactoryBot.build(:amenity, name: 'foo').should_not be_valid
-  end
-
-  context 'validated attribute' do
-    it 'should not be validated by default' do
-      FactoryBot.create(:amenity).validated?.should be false
-    end
-
-    it 'should allow validated to be set to true' do
-      amenity = FactoryBot.create(:amenity)
-      amenity.update(validated: true)
-      amenity.reload.validated?.should be true
-    end
-  end
-
-  context 'archive attribute' do
-    it 'should not be achive by default' do
-      FactoryBot.create(:amenity).archive?.should be false
-    end
-
-    it 'should allow archive to be set to true' do
-      amenity = FactoryBot.create(:amenity)
-      amenity.update(archive: true)
-      amenity.reload.archive?.should be true
-    end
+  it 'is invalid with a duplicate code' do
+    FactoryBot.build(:product, sku_code: product.sku_code).should_not be_valid
   end
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -2,46 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
   before(:each) do
-    @product = FactoryBot.create(:product)
     @warehouse = FactoryBot.create(:warehouse)
   end
 
-  attr_reader :product, :warehouse
+  attr_reader :warehouse
 
   it 'has a valid object' do
-    FactoryBot.create(:inventory, warehouse_code: warehouse.wh_code, product_code: product.sku_code).should be_valid
+    FactoryBot.create(:warehouse).should be_valid
   end
 
-  it 'is invalid without a name' do
-    FactoryBot.build(:amenity, name: nil).should_not be_valid
+  it 'is invalid without a code' do
+    FactoryBot.build(:warehouse, wh_code: nil).should_not be_valid
   end
 
-  it 'is invalid with a duplicate name' do
-    FactoryBot.create(:amenity, name: 'foo')
-    FactoryBot.build(:amenity, name: 'foo').should_not be_valid
-  end
-
-  context 'validated attribute' do
-    it 'should not be validated by default' do
-      FactoryBot.create(:amenity).validated?.should be false
-    end
-
-    it 'should allow validated to be set to true' do
-      amenity = FactoryBot.create(:amenity)
-      amenity.update(validated: true)
-      amenity.reload.validated?.should be true
-    end
-  end
-
-  context 'archive attribute' do
-    it 'should not be achive by default' do
-      FactoryBot.create(:amenity).archive?.should be false
-    end
-
-    it 'should allow archive to be set to true' do
-      amenity = FactoryBot.create(:amenity)
-      amenity.update(archive: true)
-      amenity.reload.archive?.should be true
-    end
+  it 'is invalid with a duplicate code' do
+    FactoryBot.build(:warehouse, wh_code: warehouse.wh_code).should_not be_valid
   end
 end
