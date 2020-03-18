f9a7149e8aee83a71e5c1fd58edc5478ca212b45
tanmayj28 <tanmayj28@gmail.com>
2020-03-18 21:03:28 +0530

    adds validations for sku_code, wh_code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :inventories, foreign_key: :product_code, primary_key: :sku_code
   has_many :warehouses, through: :inventories
+  validates :sku_code, length: {is: 8}, uniqueness: true
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,5 @@
 class Warehouse < ApplicationRecord
   has_many :inventories, foreign_key: :warehouse_code, primary_key: :wh_code
   has_many :products, through: :inventories
+  validates :wh_code, length: 4..16, uniqueness: true
 end
